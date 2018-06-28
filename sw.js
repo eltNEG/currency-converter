091a9116d1d7e5e9b5f052ed4b2f5bac8d854d8f
eltNEG <yorewole@gmail.com>
2018-06-29 00:11:10 +0100

    bumped cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'currencyConverter-v4';
+const staticCacheName = 'currencyConverter-v3';
 //const contentImgsCache = 'currencyConverter-imgs';
 const allCaches = [
   staticCacheName,
