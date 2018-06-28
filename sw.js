c64c6f698f6b321c5def8ef5678ec2f6043f7152
eltNEG <yorewole@gmail.com>
2018-06-28 18:54:51 +0100

    bumped up cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'currencyConverter-v0';
+const staticCacheName = 'currencyConverter-v3';
 //const contentImgsCache = 'currencyConverter-imgs';
 const allCaches = [
   staticCacheName,
@@ -15,6 +15,7 @@ self.addEventListener('install', (event) => {
         '/css/index.css',
         '/js/sw/index.js',
         "/js/idb/idb.js",
+        "https://unpkg.com/babel-standalone@6/babel.min.js"
       ]);
     })
   );
